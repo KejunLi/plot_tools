f763172c6d8d1027ee375a2fb24025700d7eaab5
KejunLi <likejun622@gmail.com>
2019-12-16 02:30:37 -0800

    Modify etot_vs_nk.py to make it more general

--- py/etot_vs_nk.py
+++ py/etot_vs_nk.py
@@ -1,21 +1,16 @@
 #!/usr/bin/env python3
 
-##################################################################
-# This program extracts data from a file that coleects total     #
-# energy. The total energy data are collected by 'grep ! *out.   #
-# Energy difference between two nearest data points of total     #
-# energy is ploted. The data points of total energy are selected #
-# to start from the second one.                                  #
-##################################################################
 import matplotlib.pyplot as plt
 import pylab as ply
 import numpy as np
 import re
+import os
 from configuration_for_plot import config_plot
 
 # read *.out to find lines with total energy and extract data from lines
-def extract_etot(file_name):
-    with open(file_name, "r") as f:
+def extract_etot(d_dir, file_name):
+    destinated_file = str(d_dir) + str(file_name)
+    with open(destinated_file, "r") as f:
         lines = f.readlines()
         saved_data = []
         # unit conversion
@@ -25,25 +20,96 @@ def extract_etot(file_name):
                 # \d +  # the integral part
                 # \.    # the decimal point
                 # \d *  # some fractional digits
-                raw_etot = re.findall(r"[-]\d+\.\d*", line) 
+                raw_etot = re.findall(r"[+-]?\d+\.\d*", line) 
                 etot = float(raw_etot[0])*ry2ev
                 saved_data.append(etot)
     return(saved_data)
+'''
+# show nk_*.out files in a designated directory and collect nk and etot
+def files_in_dir(d_dir):
+    list_files = []
+    list_nk = []
+    list_etot = []
+    for file_name in os.listdir(d_dir):
+        if "nk" in file_name and ".out" in file_name:
+            list_files.append(file_name)
+            raw_nk = re.findall(r"[+-]?\d+", file_name)
+            nk = int(raw_nk[0])
+            list_nk.append(nk)
+            list_etot.extend(extract_etot(d_dir, file_name))
+    saved_list = [list_files, list_nk, list_etot]
+    print(saved_list)
+    return(saved_list)
+
+# sort out $nk with associated Final energy
+def sort_nk_fenergy(d_dir):
+    raw_list = []
+    raw_list = files_in_dir(d_dir)
+    list_nk = raw_list[1]
+    list_etot = raw_list[2]
+    # create a sorted list of nk and etot
+    sorted_list_nk = sorted(list_nk)
+    sorted_list_etot = []
+    # sorted the list of etot according to the order of sorted_list_nk
+    for i, sorted_nk in enumerate(sorted_list_nk):
+        for j, nk in enumerate(list_nk):
+            if nk == sorted_nk:
+                sorted_list_etot.append(list_etot[j])
+    xy_plot = [sorted_list_nk, sorted_list_etot]
+    print(xy_plot)
+    return(xy_plot)
+'''
+# show nk_*.out files in a designated directory and collect nk and etot
+def files_in_dir(d_dir):
+    list_files = []
+    for file_name in os.listdir(d_dir):
+        if "nk" in file_name and ".out" in file_name:
+            list_files.append(file_name)
+    print(list_files)
+    return(list_files)
+
+# sort out $nk with associated Final energy
+def sort_nk_fenergy(d_dir):
+    list_nk = []
+    list_etot = []
+    for file_name in files_in_dir(d_dir):
+        raw_nk = re.findall(r"[+-]?\d+", file_name)
+        nk = int(raw_nk[0])
+        list_nk.append(nk)
+        list_etot.extend(extract_etot(d_dir, file_name))
+    saved_list = [list_nk, list_etot]
+
+    # create a sorted list of nk and etot
+    sorted_list_nk = sorted(list_nk)
+    sorted_list_etot = []
+
+    # sorted the list of etot according to the order of sorted_list_nk
+    for i, sorted_nk in enumerate(sorted_list_nk):
+        for j, nk in enumerate(list_nk):
+            if nk == sorted_nk:
+                sorted_list_etot.append(list_etot[j])
+    xy_plot = [sorted_list_nk, sorted_list_etot]
+    print(xy_plot)
+    return(xy_plot)
 
 
+destination = "/home/likejun/work/hBN/Ti/"
+xy = sort_nk_fenergy(destination)
+x_nk = xy[0]
+y_etot = xy[1]
 
-fname1 = "/home/KEJUNLI/work/h-BN/Mo_doped/relax_hybrid_1.out"
-fname2 = "/home/KEJUNLI/work/h-BN/Mo_doped/relax_hybrid_2.out"
-etot = extract_etot(fname1) + extract_etot(fname2)
+# fname1 = "/home/KEJUNLI/work/h-BN/Mo_doped/relax_hybrid_1.out"
+# fname2 = "/home/KEJUNLI/work/h-BN/Mo_doped/relax_hybrid_2.out"
+# etot = extract_etot(fname1) + extract_etot(fname2)
 
 # start to plot from the second data point
-starting_point = 5
-enume = range(starting_point-1, len(etot))
-x = range(starting_point, len(etot)+1)
+starting_point = 1
+num_points = range(starting_point-1, len(x_nk))
+x = range(starting_point, len(x_nk)+1)
 y = []
-for i in enume:
-    if i <= len(etot):
-        y.append(etot[i])
+for i in num_points:
+    if i <= len(x_nk):
+        y.append(y_etot[i])
 
 
 config_plot()
